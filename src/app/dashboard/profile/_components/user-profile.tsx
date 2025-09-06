b2583f3238dd00601431f2708382d425659ce943
Evans Elabo <ellaboevans@gmail.com>
2025-09-06 13:47:46 +0000

    feat(app): clear console logs

--- src/app/dashboard/profile/_components/user-profile.tsx
+++ src/app/dashboard/profile/_components/user-profile.tsx
@@ -37,8 +37,6 @@ export default function UserProfile() {
     setAvatar(newAvatar || "");
   };
 
-  console.log("User data:", user);
-
   useEffect(() => {
     if (user) {
       setProfileData((prev) => ({
