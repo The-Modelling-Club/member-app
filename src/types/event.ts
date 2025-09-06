7e134df651b48e605971fc55e2d6608f7ce6457e
Evans Elabo <ellaboevans@gmail.com>
2025-09-06 12:52:06 +0000

    feat(app): change field label

--- src/types/event.ts
+++ src/types/event.ts
@@ -4,7 +4,7 @@ export interface EventItem {
   _id: string;
   title: string;
   description: string;
-  event_image: string; // URL
+  event_img: string; // URL
   date: string; // ISO string or human-readable date
   location: string;
   status: EventStatus;
